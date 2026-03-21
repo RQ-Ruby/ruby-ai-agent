30eb8d2ef50c169baebc6d365be0fc050073282f
rich0807 <3428773947@qq.com>
2026-03-21 19:13:14 +0800

    deat:初始化项目

---
+++ src/main/java/com/ruby/rubyaiagent/RubyAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ruby.rubyaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubyAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruby/rubyaiagent/RubyAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruby.rubyaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubyAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
