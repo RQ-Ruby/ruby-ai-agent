148a15beecf6a6c218ca9392f5c3236f9196f5cc
rich0807 <3428773947@qq.com>
2026-04-08 19:34:02 +0800

    deat:MCP服务端开发1.0

--- src/test/java/com/ruby/rubyaiagent/ai/LoveAppTest.java
+++ src/test/java/com/ruby/rubyaiagent/ai/LoveAppTest.java
@@ -83,31 +83,25 @@ class LoveAppTest {
         assertNotNull(answer);
     }
 
-//    @Test
-//    void doChatWithMcp() {
-//        String chatId = UUID.randomUUID().toString();
-//        // 测试地图 MCP
-//        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
-//        String answer =  loveApp.doChatWithMcp(message, chatId);
-//    }
-
-
-
     @Test
     void doChatWithMcp() {
         String chatId = UUID.randomUUID().toString();
-        // 测试图片搜索 MCP
-        String message = "帮我搜索一些温暖的图片";
+        // 测试地图 MCP
+        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
         String answer =  loveApp.doChatWithMcp(message, chatId);
-        assertNotNull(answer);
     }
 
-    @Resource
-    private ToolCallbackProvider toolCallbackProvider;
-    @Test
-    void checkMcpToolsLoaded() {
-        Arrays.stream(toolCallbackProvider.getToolCallbacks())
-                .forEach(t -> System.out.println("Tool: " + t.getName()));
-    }
+
+
+//    @Test
+//    void doChatWithMcp() {
+//        String chatId = UUID.randomUUID().toString();
+//        // 测试图片搜索 MCP
+//        String message = "帮我搜索一些温暖的图片";
+//        String answer =  loveApp.doChatWithMcp(message, chatId);
+//        assertNotNull(answer);
+//    }
+
+
 
 }
